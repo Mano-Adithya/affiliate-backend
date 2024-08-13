77d1ec6b44ca12d6477d99bd4f5f4212ab636cde
Mano-Adithya <manoadithyap@gmail.com>
2024-08-13 17:30:54 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/backend
 npm install && npm install -g pm2 && npm uninstall bcrypt && npm install bcrypt
-cd /var/wwww/backend && pm2 start 'npm start'
+cd /var/www/backend && pm2 start 'npm start'
 sudo pm2 restart 0
